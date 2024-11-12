924adb9fea9404089cf194dc31014e88c76ab4fd
Anna Swann <aswann2@mail.umw.edu>
2024-11-12 15:38:41 -0500

    Server file should be finalized

--- Server.cpp
+++ Server.cpp
@@ -1,30 +1,85 @@
 #include "Server.h"
+#include <iostream>
+#include <cstring>
+#include <vector>
+#include <thread>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <mutex>
+#include <algorithm>
 
-/*
-Set up the server
-*/
+using namespace std;
+
 Server::Server(int port)
 {
-	serverSocket = socket(AF_INET, SOCK_STREAM, 0);
-	serverAddress.sin_family = AF_INET;
-	serverAddress.sin_port = htons(port);
-	serverAddress.sin_addr.s_addr = INADDR_ANY;
+    serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_port = htons(port);
+    serverAddress.sin_addr.s_addr = INADDR_ANY;
 }
 
-/*
-Close the server
-*/
 Server::~Server()
 {
-	close(serverSocket);
+    close(serverSocket);
+    for (int client : clientSockets)
+    {
+        close(client);
+    }
 }
 
-/*
-Boot the server
-*/
 void Server::start()
 {
-	bind(serverSocket, (struct sockaddr*)&serverAddress, 
-		sizeof(serverAddress));
-	listen(serverSocket, 5);
+    bind(serverSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress));
+    listen(serverSocket, 5);
+    cout << "Server started. Waiting for clients to connect..." << endl;
+
+    while (true)
+    {
+        addClient();
+    }
+}
+
+void Server::addClient()
+{
+    sockaddr_in clientAddress;
+    socklen_t clientAddrLen = sizeof(clientAddress);
+    int clientSocket = accept(serverSocket, (struct sockaddr*)&clientAddress, &clientAddrLen);
+
+    if (clientSocket != -1)
+    {
+        lock_guard<mutex> lock(clientMutex);
+        clientSockets.push_back(clientSocket);
+        cout << "Client connected!" << endl;
+
+        thread([this, clientSocket]() {
+            char buffer[1024];
+            while (true)
+            {
+                memset(buffer, 0, sizeof(buffer));
+                int bytesRead = recv(clientSocket, buffer, sizeof(buffer), 0);
+                if (bytesRead <= 0)
+                {
+                    close(clientSocket);
+                    lock_guard<mutex> lock(clientMutex);
+                    clientSockets.erase(std::remove(clientSockets.begin(), clientSockets.end(), clientSocket), clientSockets.end());
+                    cout << "Client disconnected." << endl;
+                    break;
+                }
+                broadcastMessage(buffer, clientSocket);
+            }
+        }).detach();
+    }
+}
+
+void Server::broadcastMessage(const string &message, int senderSocket)
+{
+    lock_guard<mutex> lock(clientMutex);
+    for (int client : clientSockets)
+    {
+        if (client != senderSocket) 
+        {
+            send(client, message.c_str(), message.length(), 0);
+        }
+    }
 }
--- Server.h
+++ Server.h
@@ -1,22 +1,26 @@
 #include <iostream>
 #include <cstring>
+#include <vector>
+#include <thread>
 #include <netinet/in.h>
 #include <sys/socket.h>
 #include <unistd.h>
+#include <mutex>
 
 using namespace std;
 
 class Server
 {
 public:
-	Server(int port);
-	~Server();
-	void start();
-	void addClient();
-	void receiveMessage();
+    Server(int port);
+    ~Server();
+    void start();
+    void addClient();
+    void broadcastMessage(const string &message, int senderSocket);
 
 private:
-	int serverSocket;
-	int clientSocket;
-	sockaddr_in serverAddress;
+    int serverSocket;
+    sockaddr_in serverAddress;
+    vector<int> clientSockets;
+    mutex clientMutex;
 };
